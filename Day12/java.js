d063fb237f07c53698e9ff65cea4f95aa71ead62
singhdeewas2-glitch <singhdeewas2@gmail.com>
2025-09-10 21:32:55 +0530

    gaaaa

---
+++ Day12/java.js
@@ -0,0 +1,10 @@
+let age=17;
+if(age>=18){
+    console.log("you are eligible to vote");
+}
+else{
+    console.log("You are not eligible to vote");
+}
+
+// output : windows:  ctrl + /
+// mac : cmd + /
